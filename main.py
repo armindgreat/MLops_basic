764cfdcdc96cd75de8311332e6642e4ad9197484
MalayZ <malayzalawadia2003@gmail.com>
2024-08-30 01:59:59 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from MLops_project import logger
 from MLops_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from MLops_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -11,3 +13,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/MLops_project/config/configuration.py
+++ src/MLops_project/config/configuration.py
@@ -1,6 +1,7 @@
 from MLops_project.constants import *
 from MLops_project.utils.common import read_yaml, create_directories
-from MLops_project.entity.config_entity import DataIngestionConfig
+from MLops_project.entity.config_entity import (DataIngestionConfig, 
+                                                DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -29,3 +30,19 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
+    
