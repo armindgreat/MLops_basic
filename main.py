75b01bbe5b97fa30846dac21aaa80233a8dd4c5c
MalayZ <malayzalawadia2003@gmail.com>
2024-08-30 02:22:48 +0530

    data tranformation done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from MLops_project import logger
 from MLops_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from MLops_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from MLops_project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -20,6 +21,18 @@ STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
